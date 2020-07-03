d612a38302cf29611d263f84f45e7076f0046c40
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2020-07-03 10:18:10 -0500

    Printing more verbose device specs

--- pystats2md/report.py
+++ pystats2md/report.py
@@ -10,9 +10,11 @@ from pathlib import Path
 import re
 
 import psutil
+import cpuinfo
 
 from pystats2md.stats_table import StatsTable
 from pystats2md.stats_plot import StatsPlot
+from pystats2md.helpers import *
 
 
 class Report(object):
@@ -24,7 +26,7 @@ class Report(object):
         self.content = ''
         self.attachments = {}
 
-    def filename_for(self, filename) -> str:        
+    def filename_for(self, filename) -> str:
         return re.sub('[^\w\-_\.]', '_', filename)
 
     def print_to(self, filename: str, overwrite=True) -> Report:
@@ -33,7 +35,8 @@ class Report(object):
         text_file.close()
 
         for title, obj in self.attachments.items():
-            obj_path = Path(filename).parent / f'{self.filename_for(title)}.svg'
+            obj_path = Path(filename).parent / \
+                f'{self.filename_for(title)}.svg'
             obj.save_to(str(obj_path))
 
         self.clear()
@@ -54,7 +57,7 @@ class Report(object):
         assert isinstance(text, str)
         # Remove whitespaces in front of every row.
         # text = '\n'.join([line.strip() for line in text.splitlines()])
-        text=inspect.cleandoc(text)
+        text = inspect.cleandoc(text)
         self.content += f'{text}\n'
         # Headers must have 2 line spacings.
         self.content += '\n\n'
@@ -68,7 +71,7 @@ class Report(object):
 
     def add_plot(self, obj: StatsPlot) -> Report:
         assert isinstance(obj, StatsPlot)
-        self.attachments[obj.title]=obj
+        self.attachments[obj.title] = obj
         self.content += '![{}]({}.svg)'.format(
             obj.title,
             self.filename_for(obj.title),
@@ -77,15 +80,20 @@ class Report(object):
         return self
 
     def add_current_device_specs(self) -> Report:
-        cores=psutil.cpu_count(logical=False)
-        threads=psutil.cpu_count(logical=True)
-        frequency=psutil.cpu_freq().min
-        ram_gbs=psutil.virtual_memory().total / (2 ** 30)
-        disk_gbs=psutil.disk_usage('/').total / (2 ** 30)
+        cpu_info = cpuinfo.get_cpu_info()
+        cpu_model = cpu_info.get('brand', '?')
+        cpu_cores = psutil.cpu_count(logical=False)
+        cpu_threads = psutil.cpu_count(logical=True)
+        cpu_frequency = metric2str(psutil.cpu_freq().min * 1e6)
+        ram_gbs = bytes2str(psutil.virtual_memory().total)
+        disk_gbs = bytes2str(psutil.disk_usage('/').total)
         self.add(f'''
-        * CPU: {cores} cores, {threads} threads @ {frequency:.2f} Mhz.
-        * RAM: {ram_gbs:.2f} Gb
-        * Disk: {disk_gbs:.2f} Gb
-        * OS: {platform.system()}
+        * CPU:
+            * Model: `{cpu_model}`.
+            * Cores: {cpu_cores} ({cpu_threads} threads @ {cpu_frequency}hz).
+        * RAM Space: {ram_gbs}.
+        * Disk Space: {disk_gbs}.
+        * OS Family: {platform.system()}.
+        * Python Version: {platform.sys.version}.
         ''')
         return self
