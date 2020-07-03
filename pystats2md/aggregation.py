8e3c0569c3c143577e38f748427aae10f772650f
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2020-07-02 21:27:38 -0500

    Accounting for empty inputs

--- pystats2md/aggregation.py
+++ pystats2md/aggregation.py
@@ -5,40 +5,60 @@ class Aggregation(object):
 
     @staticmethod
     def take_first(vals) -> object:
+        if len(vals) == 0:
+            return 0
         return vals[0]
 
     @staticmethod
     def take_last(vals) -> object:
+        if len(vals) == 0:
+            return 0
         return vals[-1]
 
     @staticmethod
     def take_min(vals) -> float:
+        if len(vals) == 0:
+            return 0
         return min(vals)
 
     @staticmethod
     def take_max(vals) -> float:
+        if len(vals) == 0:
+            return 0
         return max(vals)
 
     @staticmethod
     def take_sum(vals) -> float:
+        if len(vals) == 0:
+            return 0
         return sum(vals)
 
     @staticmethod
     def take_mean(vals) -> float:
+        if len(vals) == 0:
+            return 0
         return statistics.mean(vals)
 
     @staticmethod
     def take_median(vals) -> float:
+        if len(vals) == 0:
+            return 0
         return statistics.median(vals)
 
     @staticmethod
     def take_mode(vals) -> float:
+        if len(vals) == 0:
+            return 0
         return statistics.mode(vals)
 
     @staticmethod
     def take_stdev(vals) -> float:
+        if len(vals) == 0:
+            return 0
         return statistics.stdev(vals)
 
     @staticmethod
     def take_variance(vals) -> float:
+        if len(vals) == 0:
+            return 0
         return statistics.variance(vals)
