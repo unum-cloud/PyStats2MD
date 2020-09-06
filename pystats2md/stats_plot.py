9c16ee2de3ba8b6009590cfbdaecd309daff85ea
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2020-09-06 17:29:09 +0400

    Finer control over Plotly chart layout

--- pystats2md/stats_plot.py
+++ pystats2md/stats_plot.py
@@ -8,34 +8,43 @@ from pystats2md.helpers import *
 
 class StatsPlot(object):
 
-    def __init__(self, table, title: str, show_values=False):
+    def __init__(self, table, title: str, print_height=True, print_cols_names=True, print_rows_names=True):
         self.table = table
         self.title = title
-        self.show_values = show_values
+        self.print_height = print_height
+        self.print_cols_names = print_cols_names
+        self.print_rows_names = print_rows_names
 
     def save_to(self, path: str = '') -> str:
-        comparisons = self.table.header_row
-        variants = self.table.header_col
+        cols_names = self.table.header_row if self.print_cols_names else ([''] * len(self.table.header_row))
+        rows_names = self.table.header_col if self.print_rows_names else ([''] * len(self.table.header_col))
         availiable_colors = px.colors.qualitative.Prism
 
         fig = go.Figure()
-        for i, v in enumerate(variants):
+        for row_idx, row_name in enumerate(rows_names):
             fig.add_trace(go.Bar(
-                name=v,
-                x=comparisons,
+                name=row_name,
+                x=cols_names,
                 #
-                y=self.table.content[i],
-                marker_color=availiable_colors[i % len(availiable_colors)],
+                y=self.table.content[row_idx],
+                marker_color=availiable_colors[row_idx % len(availiable_colors)],
                 #
-                text=(self.table.content[i] if self.show_values else None),
-                texttemplate=('%{text:.2s}' if self.show_values else None),
+                text=(self.table.content[row_idx] if self.print_height else None),
+                texttemplate=('%{text:.2s}' if self.print_height else None),
                 textposition='auto',
             ))
 
         # https://plotly.com/python/bar-charts/
+        # https://plotly.com/python/tick-formatting/
+        # https://plotly.com/python/axes/
         fig.update_layout(
             title_text=self.title,
             #
+            # xaxis_tickfont=dict(
+            #     family='Rockwell', 
+            #     color='crimson', 
+            #     size=14,                
+            # ),
             xaxis_tickangle=-45,
             uniformtext_minsize=8,
             uniformtext_mode='hide',
